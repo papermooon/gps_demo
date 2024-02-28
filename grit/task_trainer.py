b747aa04bcff8e84d0115c7f684451be5ae2d281
Guo Muyuan <631803439@qq.com>
2024-02-28 11:15:58 +0800

    改模型为二分类

--- grit/task_trainer.py
+++ grit/task_trainer.py
@@ -11,6 +11,33 @@ import torch.nn.functional as F
 from sklearn.metrics import classification_report, matthews_corrcoef
 
 
+class MultiClassFocalLossWithAlpha(nn.Module):
+    def __init__(self, alpha=[0.3, 0.6, 0.1], gamma=2, reduction='mean'):
+        """
+        :param alpha: 权重系数列表，三分类中第0类权重0.2，第1类权重0.3，第2类权重0.5
+        :param gamma: 困难样本挖掘的gamma
+        :param reduction:
+        """
+        super(MultiClassFocalLossWithAlpha, self).__init__()
+        self.alpha = torch.tensor(alpha, device='cuda')
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, pred, target):
+        alpha = self.alpha[target]  # 为当前batch内的样本，逐个分配类别权重，shape=(bs), 一维向量
+        log_softmax = torch.log_softmax(pred, dim=1)  # 对模型裸输出做softmax再取log, shape=(bs, 3)
+        logpt = torch.gather(log_softmax, dim=1, index=target.view(-1, 1))  # 取出每个样本在类别标签位置的log_softmax值, shape=(bs, 1)
+        logpt = logpt.view(-1)  # 降维，shape=(bs)
+        ce_loss = -logpt  # 对log_softmax再取负，就是交叉熵了
+        pt = torch.exp(logpt)  # 对log_softmax取exp，把log消了，就是每个样本在类别标签位置的softmax值了，shape=(bs)
+        focal_loss = alpha * (1 - pt) ** self.gamma * ce_loss  # 根据公式计算focal loss，得到每个样本的loss值，shape=(bs)
+        if self.reduction == "mean":
+            return torch.mean(focal_loss)
+        if self.reduction == "sum":
+            return torch.sum(focal_loss)
+        return focal_loss
+
+
 class TaskTrainer:
 
     def __init__(self, model, output_dir, grad_norm_clip=1.0, device='cuda',
@@ -30,7 +57,8 @@ class TaskTrainer:
             self.loss_fn = nn.MSELoss()
         elif task_type == 'classification':
             # self.loss_fn = nn.BCEWithLogitsLoss()
-            self.loss_fn = nn.CrossEntropyLoss()
+            # self.loss_fn = nn.CrossEntropyLoss()
+            self.loss_fn = MultiClassFocalLossWithAlpha()
         else:
             raise Exception(f'Unknown task type: {task_type}')
 
@@ -80,8 +108,10 @@ class TaskTrainer:
         # print(true)
         # pred = F.log_softmax(pred, dim=-1)
         # loss = F.nll_loss(pred.squeeze(), true.squeeze())
+
         loss = self.loss_fn(pred, true)
         pred = torch.sigmoid(pred)
+
         return loss, pred, true
 
     def train_epoch(self, epoch, model, train_loader):
